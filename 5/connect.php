faea7ab4c18199e4ed7b7227ed07efd6e439f95f
39692-spec <39692@bpk.ac.th>
2026-09-18 19:42:26 +0700

    Add files via upload

---
+++ 5/connect.php
@@ -0,0 +1,16 @@
+<?php
+$host = "sql206.infinityfree.com";
+$username = "if0_42881913";
+$password = "234123567gus";
+$dbname = "if0_42881913_workshop";
+$conn = new mysqli($host, $username, $password, $dbname);
+    if ($conn->connect_error) 
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+$conn->set_charset("utf8mb4");
+    if (basename($_SERVER['SCRIPT_FILENAME']) === 'connect.php')
+        {
+        echo "เชื่อมต่อสำเร็จ นายฐิติวัชร์	เลิศทักษิณานนท์";
+        }
+?>
